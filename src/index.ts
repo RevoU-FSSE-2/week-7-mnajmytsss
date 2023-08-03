d650fd2ad24e8b9a623a68611d98e58a082f0424
mnajmytsss <najmytsaqiby22@gmail.com>
2023-08-04 03:18:13 +0700

    chore: add script dom

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 const formInput = document.getElementById("form");
 formInput?.addEventListener("submit", (input) => {
-    input.preventDefault();
-    var cash = (<HTMLInputElement>document.getElementById("cash")).value;
-    var name = (<HTMLInputElement>document.getElementById("inputName")).value;
-    var details = (<HTMLInputElement>document.getElementById("inputDetails")).value;
-    var amount = (<HTMLInputElement>document.getElementById("inputAmount")).value;
-    console.log(`${cash}, ${name}, ${details}, ${amount}`)
-}) 
+  input.preventDefault();
+  let cash = (<HTMLInputElement>document.getElementById("cash")).value;
+  let name = (<HTMLInputElement>document.getElementById("inputName")).value;
+  let details = (<HTMLInputElement>document.getElementById("inputDetails"))
+    .value;
+  let amount = (<HTMLInputElement>document.getElementById("inputAmount")).value;
+  console.log(`${cash}, ${name}, ${details}, ${amount}`);
 
+  let HTMLElement = document.getElementById("biji");
+  let newElement = document.createElement("h3");
+  newElement.textContent = `${cash}`;
+  HTMLElement?.appendChild(newElement);
 
-
+  let newElement2 = document.createElement("h4");
+  newElement2.textContent = `${name} Telah Melakukan ${details} Sebesar Rp. ${amount}`;
+  HTMLElement?.appendChild(newElement2);
+});
