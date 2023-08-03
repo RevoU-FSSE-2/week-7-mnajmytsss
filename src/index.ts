176961b6b4416ec4df6b6f22f41f632faa74bc5e
mnajmytsss <najmytsaqiby22@gmail.com>
2023-08-03 17:34:34 +0700

    add assignment script

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-console.log('hello world')
-console.log("biji2")
+const formInput = document.getElementById("form");
+formInput?.addEventListener("submit", (input) => {
+    input.preventDefault();
+    var cash = (<HTMLInputElement>document.getElementById("cash")).value;
+    var name = (<HTMLInputElement>document.getElementById("inputName")).value;
+    var details = (<HTMLInputElement>document.getElementById("inputDetails")).value;
+    var amount = (<HTMLInputElement>document.getElementById("inputAmount")).value;
+    console.log(`${cash}, ${name}, ${details}, ${amount}`)
+}) 
+
+
+
