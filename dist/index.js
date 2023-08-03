1d39aa501231b61ef1c3be3a08a781302b18633b
mnajmytsss <najmytsaqiby22@gmail.com>
2023-08-03 17:35:28 +0700

    convert index.ts to index.js

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log('hello world');
-console.log("biji2");
+const formInput = document.getElementById("form");
+formInput === null || formInput === void 0 ? void 0 : formInput.addEventListener("submit", (input) => {
+    input.preventDefault();
+    var cash = document.getElementById("cash").value;
+    var name = document.getElementById("inputName").value;
+    var details = document.getElementById("inputDetails").value;
+    var amount = document.getElementById("inputAmount").value;
+    console.log(`${cash}, ${name}, ${details}, ${amount}`);
+});
