de8bb50de04f166c2dada04e2f5f2c99b0f9ce67
mnajmytsss <najmytsaqiby22@gmail.com>
2023-08-04 03:17:20 +0700

    chore: converting from index.ts

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,17 @@
 const formInput = document.getElementById("form");
 formInput === null || formInput === void 0 ? void 0 : formInput.addEventListener("submit", (input) => {
     input.preventDefault();
-    var cash = document.getElementById("cash").value;
-    var name = document.getElementById("inputName").value;
-    var details = document.getElementById("inputDetails").value;
-    var amount = document.getElementById("inputAmount").value;
+    let cash = document.getElementById("cash").value;
+    let name = document.getElementById("inputName").value;
+    let details = document.getElementById("inputDetails")
+        .value;
+    let amount = document.getElementById("inputAmount").value;
     console.log(`${cash}, ${name}, ${details}, ${amount}`);
+    let HTMLElement = document.getElementById("biji");
+    let newElement = document.createElement("h3");
+    newElement.textContent = `${cash}`;
+    HTMLElement === null || HTMLElement === void 0 ? void 0 : HTMLElement.appendChild(newElement);
+    let newElement2 = document.createElement("h4");
+    newElement2.textContent = `${name} Telah Melakukan ${details} Sebesar Rp. ${amount}`;
+    HTMLElement === null || HTMLElement === void 0 ? void 0 : HTMLElement.appendChild(newElement2);
 });
